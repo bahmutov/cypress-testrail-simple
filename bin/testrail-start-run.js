88043476b645fc8c8796535d1cb6e08399383433
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-07 15:03:27 -0400

    read the name and description

--- bin/testrail-start-run.js
+++ bin/testrail-start-run.js
@@ -6,6 +6,10 @@ const debug = require('debug')('cypress-testrail-simple')
 const got = require('got')
 const { getTestRailConfig } = require('../src/get-config')
 
+// optional arguments
+const name = process.argv[2]
+const description = process.argv[3]
+
 const testRailInfo = getTestRailConfig()
 
 // only output the run ID to the STDOUT, everything else is logged to the STDERR
@@ -24,6 +28,10 @@ got(addRunUrl, {
   headers: {
     'Content-Type': 'application/json',
     authorization,
+  },
+  json: {
+    name,
+    description,
   },
 })
   .json()
