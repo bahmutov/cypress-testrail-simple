7287855c94a740c4b449ee01b821888d35931f93
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-08 08:59:31 -0400

    handle run closed in parallel

--- bin/testrail-close-run.js
+++ bin/testrail-close-run.js
@@ -26,6 +26,11 @@ debug('test rail info without the password')
 debug('%o', { ...testRailInfo, password: '***' })
 
 getTestRun(runId, testRailInfo).then((runInfo) => {
+  if (runInfo.is_completed) {
+    console.log('Run %d is already completed', runId)
+    return
+  }
+
   const allTestsDone = runInfo.untested_count === 0
   if (!allTestsDone) {
     console.log(
@@ -34,21 +39,30 @@ getTestRun(runId, testRailInfo).then((runInfo) => {
       runInfo.untested_count,
     )
     console.log('not closing the run...')
-  } else {
-    closeTestRun(runId, testRailInfo).then(
-      (json) => {
-        console.log('Closed run %d', json.id)
-        console.log('name: %s', json.name)
-        console.log('description: %s', json.description)
-        console.log('passed tests: %d', json.passed_count)
-        console.log('failed tests: %d', json.failed_count)
-        // untested count should be zero
-        console.log('untested: %d', json.untested_count)
-      },
-      (error) => {
+    return
+  }
+  closeTestRun(runId, testRailInfo).then(
+    (json) => {
+      console.log('Closed run %d', json.id)
+      console.log('name: %s', json.name)
+      console.log('description: %s', json.description)
+      console.log('passed tests: %d', json.passed_count)
+      console.log('failed tests: %d', json.failed_count)
+      // untested count should be zero
+      console.log('untested: %d', json.untested_count)
+    },
+    (error) => {
+      // the error might be legit error, or the run was closed
+      // while we were checking its status, let's try again
+      return getTestRun(runId, testRailInfo).then((runInfo) => {
+        if (runInfo.is_completed) {
+          console.log('Run %d was already completed', runId)
+          return
+        }
+        console.error('original message when trying to close the run')
         console.error(error)
         process.exit(1)
-      },
-    )
-  }
+      })
+    },
+  )
 })
