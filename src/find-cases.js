dd9eae3b63d81b9ebbd27fde2034892a29dc9967
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-13 10:44:26 -0400

    find in each spec

--- src/find-cases.js
+++ src/find-cases.js
@@ -1,8 +1,14 @@
 const fs = require('fs')
-function findCases(spec) {
+
+function findCasesInSpec(spec) {
   const source = fs.readFileSync(spec, 'utf8')
   const matches = source.match(/C\d+/g)
   return matches.map((m) => m.slice(1)).map(Number)
+}
+
+function findCases(specs) {
+  // find case Ids in each spec and flatten into a single array
+  return specs.map(findCasesInSpec).reduce((a, b) => a.concat(b), [])
 }
 
 module.exports = { findCases }
