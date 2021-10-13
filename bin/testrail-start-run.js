7b816095eec36cca98bfe01143e10332447998be
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-13 10:31:16 -0400

    print found specs

--- bin/testrail-start-run.js
+++ bin/testrail-start-run.js
@@ -5,6 +5,7 @@
 const arg = require('arg')
 const debug = require('debug')('cypress-testrail-simple')
 const got = require('got')
+const globby = require('globby')
 const { getTestRailConfig, getAuthorization } = require('../src/get-config')
 
 const args = arg(
@@ -27,6 +28,13 @@ debug('run name: %s', name)
 debug('run description: %s', description)
 
 const testRailInfo = getTestRailConfig()
+
+function findSpecs(pattern) {
+  // @ts-ignore
+  return globby(pattern, {
+    absolute: true,
+  })
+}
 
 function startRun(testRailInfo, name, description) {
   // only output the run ID to the STDOUT, everything else is logged to the STDERR
@@ -51,14 +59,21 @@ function startRun(testRailInfo, name, description) {
   }).json()
 }
 
-startRun(testRailInfo, name, description).then(
-  (json) => {
-    debug('response from the close_run')
-    debug('%o', json)
-    console.log(json.id)
-  },
-  (error) => {
-    console.error(error)
-    process.exit(1)
-  },
-)
+if (args['--spec']) {
+  findSpecs(args['--spec']).then((specs) => {
+    debug('using pattern "%s" found specs', args['--spec'])
+    debug(specs)
+  })
+} else {
+  startRun(testRailInfo, name, description).then(
+    (json) => {
+      debug('response from the close_run')
+      debug('%o', json)
+      console.log(json.id)
+    },
+    (error) => {
+      console.error(error)
+      process.exit(1)
+    },
+  )
+}
