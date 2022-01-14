389e28673922641b9f8e21ca8bbeb9acc10a6b8a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-14 14:59:42 -0500

    fix: improve test case detection against spurious matches (#22)

---
+++ cypress/integration/find-cases-spec.js
@@ -0,0 +1,83 @@
+/// <reference types="cypress" />
+import { findCases, findCasesInSpec } from '../../src/find-cases'
+
+describe('Finding test case IDs', () => {
+  it('finds test IDs in a single spec', () => {
+    const source = `
+      it('C1 works', () => {
+        cy.wait(15000)
+      })
+
+      it('C2002 works', () => {
+        cy.wait(15000)
+      })
+
+      it('new case C199', () => {
+        cy.wait(15000)
+      })
+    `
+    const readFile = cy.stub().returns(source)
+    const filename = 'test-spec.js'
+    const ids = findCasesInSpec(filename, readFile)
+    expect(ids, 'test cases').to.deep.equal([1, 2002, 199])
+    expect(readFile).to.be.calledOnceWithExactly(filename, 'utf8')
+  })
+
+  it('finds test IDs from all files', () => {
+    const source1 = `
+      it('C1 works', () => {
+        cy.wait(15000)
+      })
+    `
+
+    const source2 = `
+      it('not test id', () => {
+        cy.wait(15000)
+      })
+    `
+
+    const source3 = `
+      it('new case C199', () => {
+        cy.wait(15000)
+      })
+    `
+    const readFile = cy.stub()
+    readFile.withArgs('file1.js', 'utf8').returns(source1)
+    readFile.withArgs('file2.js', 'utf8').returns(source2)
+    readFile.withArgs('file3.js', 'utf8').returns(source3)
+    const ids = findCases(['file1.js', 'file2.js', 'file3.js'], readFile)
+    expect(ids, 'test cases').to.deep.equal([1, 199])
+  })
+
+  it('returns an empty list', () => {
+    const source = `
+      it('has no test id')
+    `
+    const readFile = cy.stub().returns(source)
+    const filename = 'test-spec.js'
+    const ids = findCasesInSpec(filename, readFile)
+    expect(ids, 'test cases').to.deep.equal([])
+  })
+
+  it('ignores stray Cs', () => {
+    const source = `
+      // some link /foo/C1
+      // another linke C2/
+    `
+    const readFile = cy.stub().returns(source)
+    const filename = 'test-spec.js'
+    const ids = findCasesInSpec(filename, readFile)
+    expect(ids, 'test cases').to.deep.equal([])
+  })
+
+  it('can be in different quotes', () => {
+    const source = `
+      "C1"
+      'C2'
+    `
+    const readFile = cy.stub().returns(source)
+    const filename = 'test-spec.js'
+    const ids = findCasesInSpec(filename, readFile)
+    expect(ids, 'test cases').to.deep.equal([1, 2])
+  })
+})
