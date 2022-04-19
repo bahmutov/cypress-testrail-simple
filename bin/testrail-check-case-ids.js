a17d96cf568fa96a4f7c3600ac868f5624616f47
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-04-19 11:43:13 -0400

    feat: add option --find-specs to the check script

--- bin/testrail-check-case-ids.js
+++ bin/testrail-check-case-ids.js
@@ -2,14 +2,42 @@
 
 // @ts-check
 
+const fs = require('fs')
 const arg = require('arg')
+const debug = require('debug')('cypress-testrail-simple')
+const findCypressSpecs = require('find-cypress-specs')
+
+const { findCases } = require('../src/find-cases')
 const { getTestRailConfig } = require('../src/get-config')
 const { getProjectCases } = require('../src/get-project-cases')
 require('console.table')
 
+const args = arg(
+  {
+    // find the specs automatically using
+    // https://github.com/bahmutov/find-cypress-specs
+    '--find-specs': Boolean,
+  },
+  { permissive: true },
+)
+// optional arguments
+debug('args: %o', args)
+
 const testRailInfo = getTestRailConfig()
 getProjectCases({ testRailInfo }).then((list) => {
-  console.table(list)
-  console.log()
-  console.log('%d cases found', list.length)
+  debug('TestRail has %d cases', list.length)
+  if (args['--find-specs']) {
+    const specs = findCypressSpecs.getSpecs()
+    debug('found %d Cypress specs', specs.length)
+    const caseIds = findCases(specs, fs.readFileSync)
+    debug(
+      'found %d TestRail case ids in spec files: %o',
+      caseIds.length,
+      caseIds,
+    )
+  } else {
+    console.table(list)
+    console.log()
+    console.log('%d cases found', list.length)
+  }
 })
